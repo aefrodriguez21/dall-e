2201ef98cddcf0c78516585149a982a2d11ace89
aefrodriguez21 <ae.frodriguez21@hotmail.com>
2024-04-02 00:06:41 -0600

    ninth commit

--- server/index.js
+++ server/index.js
@@ -9,14 +9,7 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-
-const corsOptions = {
-    origin: 'https://dall-e-web-78mh.onrender.com',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: true,
-    optionsSuccessStatus: 204,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
