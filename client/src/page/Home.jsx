fd08830996d1f9000ca0989785e5600bbb1be06c
aefrodriguez21 <ae.frodriguez21@hotmail.com>
2024-04-01 23:22:23 -0600

    Sixth commit

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://dall-e-rzmq.onrender.com/api/v1/post', {
+      const response = await fetch('https://dall-e-web-78mh.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
